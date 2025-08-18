075f57f009121feb25a186e3ba7dc7d7c389f03a
braulio1309 <brauliozapatad@gmail.com>
2025-08-17 23:37:19 -0400

    new fields

---
+++ database/migrations/2025_08_18_033539_add_album_20x20_photos_to_galerias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->integer('album_20x20_photos')->default(0)->after('show_complete_session_button');
+            $table->boolean('includes_canvas_60x40')->after('album_20x20_photos');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            //
+        });
+    }
+};
